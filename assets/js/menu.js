8c2e9dc986504f1fb87dfc8e5c344c32653b1551
cdpaez <archivos1789@gmail.com>
2025-04-04 10:31:43 -0500

    enfoque mobiel first

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,11 +1,16 @@
-const menuToggle = document.getElementById("menu-toggle");
-const cerrarMenu = document.getElementById("cerrar-menu");
-const navbar = document.getElementById("navbar");
+const toggleBtn = document.getElementById('menu-toggle');
+const navbar = document.getElementById('navbar');
+const menuIcon = toggleBtn.querySelector('i'); // Aquí pescamos el <i> dentro del botón
 
-menuToggle.addEventListener("click", () => {
-    navbar.classList.add("active");
-});
+toggleBtn.addEventListener('click', () => {
+  navbar.classList.toggle('active');
 
-cerrarMenu.addEventListener("click", () => {
-    navbar.classList.remove("active");
+  // Cambiar el ícono del botón (hamburguesa <-> X)
+  if (navbar.classList.contains('active')) {
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-xmark');
+  } else {
+    menuIcon.classList.remove('fa-xmark');
+    menuIcon.classList.add('fa-bars');
+  }
 });
